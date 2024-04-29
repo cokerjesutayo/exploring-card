52853edea1065235bf0807c836beaabf57943df1
cokerjesutayo <joyscoker20@gmail.com>
2024-04-29 15:00:25 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const panels=document.querySelectorAll(".panel");
+const removeActiveClass=function(){
+    panels.forEach((panel)=>{
+        panel.classList .remove("active");
+    });
+};
+panels.forEach((panel)=>{
+    panel.addEventListener("click",()=>{
+        removeActiveClass();
+        panel.classList.add("active");
+    });
+});
